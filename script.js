6a6d88e37a18f06c93483eedd7ea2b5cdb7f66ee
AntoniaArjMor <an.arjona@duocuc.cl>
2025-06-26 19:52:48 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let numero=10;
+
+console.log("es"+numero+">"+5)
+console.log("respuest: ", numero >5);
+
+if(numero>5){
+    console.log("el numero es mayor")
+}else if(numero <5){
+    console.log("El numero es menor que 5")
+}else{//resultado por default
+    console.log("El número es igual a 5")
+}
